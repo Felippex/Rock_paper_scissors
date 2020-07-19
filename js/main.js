6ad369fbc72fdd3862a784355fd9b6d5a941fe71
Felippex <55957871+Felippex@users.noreply.github.com>
2020-07-19 20:39:34 +0200

    add publication and checkResults, function endGame

--- js/main.js
+++ js/main.js
@@ -25,6 +25,46 @@ function aiChoice() {
   return aiHand
 }
 
+function checkResult(player, ai) {
+  if (player === ai) {
+    return 'draw'
+  } else if ((player === "paper" && ai === 'rock') || (player === "rock" && ai === "scissors") || (player === 'scissors' && ai === 'paper')) {
+    return 'win'
+
+  } else {
+    return "loss"
+
+  }
+}
+
+//Results
+function publishResult(player, ai, result) {
+  document.querySelector('[data-summary="your-choice"]').textContent = player
+  document.querySelector('[data-summary="ai-choice"]').textContent = ai
+
+  document.querySelector('p.numbers span').textContent = ++gameSummary.numbers
+
+  if (result === "win") {
+    document.querySelector('p.wins span').textContent = ++gameSummary.wins
+    document.querySelector('[data-summary="who-win"]').textContent = "You win!"
+    document.querySelector('[data-summary="who-win"]').style.color = 'green'
+  } else if (result === "loss") {
+    document.querySelector('p.losses span').textContent = ++gameSummary.losses
+    document.querySelector('[data-summary="who-win"]').textContent = "You lose"
+    document.querySelector('[data-summary="who-win"]').style.color = 'red'
+  } else {
+    document.querySelector('p.draws span').textContent = ++gameSummary.draws
+    document.querySelector('[data-summary="who-win"]').textContent = "Draw"
+    document.querySelector('[data-summary="who-win"]').style.color = 'blue'
+  }
+}
+
+function endGame() {
+  document.querySelector(`[data-option="${game.playerHand}"]`).style.boxShadow = '';
+  game.playerHand = '';
+  game.aiHand = '';
+}
+
 // game function
 function startGame() {
   if (!game.playerHand) {
@@ -35,8 +75,25 @@ function startGame() {
   console.log(gameResult);
   publishResult(game.playerHand, game.aiHand, gameResult);
 
+  endGame();
 }
 
 
 hands.forEach(hand => hand.addEventListener('click', handSelection))
 document.querySelector('.start').addEventListener('click', startGame)
+
+function resetResults() {
+  document.querySelector('p.wins span').textContent = "0";
+  document.querySelector('p.losses span').textContent = "0";
+  document.querySelector('p.draws span').textContent = "0";
+  document.querySelector('p.numbers span').textContent = "0";
+  document.querySelector('[data-summary="who-win"]').textContent = "";
+  document.querySelector('[data-summary="your-choice"]').textContent = "";
+  document.querySelector('[data-summary="ai-choice"]').textContent = "";
+  gameSummary.numbers = 0;
+  gameSummary.wins = 0;
+  gameSummary.losses = 0;
+  gameSummary.draws = 0;
+}
+
+document.querySelector('.reset').addEventListener('click', resetResults)
